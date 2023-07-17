bc09fb9f594c42181f99645cce12fc5a1041dc0f
SsChany <mashimaro501@naver.com>
2023-07-17 09:29:05 +0900

    chap65_mybatis_restapi

---
+++ src/main/java/com/javalab/restapi/service/UserService.java
@@ -0,0 +1,20 @@
+package com.javalab.restapi.service;
+
+import java.util.List;
+
+import com.javalab.restapi.vo.User;
+
+
+
+
+public interface UserService {
+
+	List<User> getUserList();
+	User getUser(Integer id);
+	Integer register(User userUser);
+	Integer register2(User user);
+	Integer modify(User user);
+	Integer remove(Integer id);
+	User login(User user);
+
+}
